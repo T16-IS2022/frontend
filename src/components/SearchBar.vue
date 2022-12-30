375d8ecc343b5605771acf078dd4d95e79c65877
Rafiu Hossain <rafiuhoss888@gmail.com>
2022-12-30 18:11:32 +0100

    Push iniziale

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <input type="text" v-model="query" placeholder="Cerca..." />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        query: ''
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  input[type="text"] {
+    width: 60%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 50px;
+  }
+  </style>
